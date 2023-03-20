8460cf51c203285382d3049f9fb0fe29548df2cb
Frankc <63744530+francescocirulli@users.noreply.github.com>
2023-03-20 17:48:22 +0100

    Update IDonationManager.sol

--- IDonationManager.sol
+++ IDonationManager.sol
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0;
-
+/**
+ * @title IDonationManager
+ * @dev Interface for the DonationManager contract.
+ */
 interface IDonationManager {
     event DistributedDonation(address indexed whitelistedReceiver, uint256 amount, uint256 distributionBlock);
     event EmergencyWithdrawn(address indexed emergencyReceiver, uint256 amount, uint256 distributionBlock);
